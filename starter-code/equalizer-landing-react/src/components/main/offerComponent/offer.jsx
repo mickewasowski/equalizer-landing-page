b375440234e98ffb67f5ebae0b15300fb92e779d
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-07-12 18:30:32 +0300

    migrated desktop offer

--- starter-code/equalizer-landing-react/src/components/main/offerComponent/offer.jsx
+++ starter-code/equalizer-landing-react/src/components/main/offerComponent/offer.jsx
@@ -1,35 +1,38 @@
 import {ReactComponent as AndroidLogo} from '../../../assets/icon-android.svg';
 import {ReactComponent as AppleLogo} from '../../../assets/icon-apple.svg';
+import appimage from '../../../assets/illustration-app.png'
+
+import {OfferContainer, AppIllustration, AppImg, PlanContainer,PlanItems, H2, Parag, PriceParag, PriceStrong, BtnContainer, AppleBtn, AndroidBtn} from './offer.styles';
 
 function Offer(){
     return(
-        <div class="offer-container">
-            <div id="app-illustration">
-                <img src="./assets/illustration-app.png" alt="app-illustration" />
-            </div>
-            <div id="plan-container">
-                <div id="plan-heading">
-                    <h2>Premium EQ</h2>
-                    <p>Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take
-                    your listening experience to a whole new level and access all our incredible features!</p>
-                </div>
-                <div id="price">
-                    <p>
-                        <strong>$4</strong>&nbsp;/&nbsp;month
-                    </p>
-                </div>
-                <div id="download-btns">
-                    <button id="apple">
+        <OfferContainer>
+            <AppIllustration>
+                <AppImg src={appimage} alt="app-illustration" />
+            </AppIllustration>
+            <PlanContainer>
+                <PlanItems>
+                    <H2>Premium EQ</H2>
+                    <Parag>Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take
+                    your listening experience to a whole new level and access all our incredible features!</Parag>
+                </PlanItems>
+                <PlanItems>
+                    <PriceParag>
+                        <PriceStrong>$4</PriceStrong>&nbsp;/&nbsp;month
+                    </PriceParag>
+                </PlanItems>
+                <BtnContainer>
+                    <AppleBtn>
                         <AppleLogo />
                         iOS Download
-                    </button>
-                    <button id="android">
+                    </AppleBtn>
+                    <AndroidBtn>
                         <AndroidLogo />
                         Android Download
-                    </button>
-                </div>
-            </div>
-        </div>
+                    </AndroidBtn>
+                </BtnContainer>
+            </PlanContainer>
+        </OfferContainer>
     )
 }
 
---
+++ starter-code/equalizer-landing-react/src/components/main/offerComponent/offer.styles.jsx
@@ -0,0 +1,113 @@
+import styled from "styled-components";
+
+export const OfferContainer = styled.div`
+background-image: url('./assets/bg-pattern-2.svg');
+    background-repeat: no-repeat;
+    background-position: 45% -30%;
+    background-color: hsl(244, 23%, 12%);
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-around;
+    margin-top: 20rem;
+`
+
+export const AppIllustration = styled.div`
+    transform: translate(0, -214px);
+`
+
+export const AppImg = styled.img`
+    width: 19.5rem;
+    height: 40.125rem;
+`
+
+export const PlanContainer = styled.div`
+    width: 27.9rem;
+    height: 39rem;
+    background-color: hsl(12, 94%, 65%);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    transform: translate(0, 115px);
+`
+
+export const PlanItems = styled.div`
+    width: 80%;
+`
+
+export const H2 = styled.h2`
+    font-size: 2.5rem;
+    color: hsl(20, 33%, 98%);
+    text-align: left;
+`
+
+export const Parag = styled.p`
+    font-size: 1.25rem;
+    color: hsl(20, 33%, 98%);
+    text-align: left;
+`
+
+export const PriceParag = styled.p`
+    display: flex;
+    align-items: center;
+    color: hsl(20, 33%, 98%);
+`
+
+export const PriceStrong = styled.strong`
+    font-size: 4rem;
+    margin-right: .8rem;
+`
+
+export const BtnContainer = styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 30%;
+    justify-content: space-around;
+`
+
+export const AppleBtn = styled.button`
+    width: 100%;
+    height: 4rem;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+    font-weight: 700;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: .4rem;
+
+    background-color: hsl(244, 23%, 12%);
+    color: hsl(20, 33%, 98%);
+
+    &:hover{
+        cursor: pointer;
+        background-color: hsl(177, 68%, 64%);
+    }
+`
+
+export const AndroidBtn = styled.button`
+    width: 100%;
+    height: 4rem;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+    font-weight: 700;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: .4rem;
+
+    background-color: hsl(20, 33%, 98%);
+    color: hsl(244, 23%, 12%);
+
+    &:hover{
+        cursor: pointer;
+        background-color: hsl(33, 100%, 70%);
+    }
+`
